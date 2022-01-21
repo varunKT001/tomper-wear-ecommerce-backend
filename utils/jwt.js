f7df4653a98617d2fdb347f5a3e5dbe7af667012
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-01-21 16:22:31 +0530

    samesite and secure options added to cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,3 +1,5 @@
+const nodemon = require('nodemon');
+
 // create jwt token and save as a cookie
 exports.sendToken = (admin, statusCode, res) => {
   const token = admin.getJwtToken();
@@ -6,6 +8,8 @@ exports.sendToken = (admin, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'none',
+    secure: true,
   };
   res
     .status(statusCode)
