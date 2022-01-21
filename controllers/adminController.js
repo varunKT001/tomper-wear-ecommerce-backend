1e7fd3a531fa4c049054f897966399bb3cb3a968
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-01-21 16:48:34 +0530

    logout route cookie options fixed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -46,6 +46,8 @@ exports.logoutAdmin = catchAsyncError(async (req, res, next) => {
   res.cookie('token', null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    sameSite: 'none',
+    secure: true,
   });
   res.status(200).json({
     success: true,
